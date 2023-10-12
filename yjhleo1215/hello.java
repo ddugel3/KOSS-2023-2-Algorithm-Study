09a8f7ddec2fc8c880d5ff73c47a1c084bfebb3c
yjhleo1215 <yjhleo1215@kookmin.ac.kr>
2023-10-12 19:50:40 +0900

    Feat: Create hello.java

---
+++ yjhleo1215/hello.java
@@ -0,0 +1,12 @@
+package yjhleo1215;
+
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hello");
+        int A = sc.nextInt();
+        System.out.println(A);
+    }
+}
